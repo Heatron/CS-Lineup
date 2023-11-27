088a4fe7646132d5c78698a6cb0cf7a4c4317726
Heatron <71969030+Heatron@users.noreply.github.com>
2023-11-27 12:08:42 +0000

    NEW
    NEW

--- nukelineup.py
+++ nukelineup.py
@@ -1,31 +1,35 @@
 "de_nuke": [ #mapname
         {
-            "stand_position": (-269.918823 -1982.502441 -351.968750), # POSITION WHERE PLAYER SHOULD STAND
-            "throw_position": (-53.481873 -1973.761841 -346.611389), #WHERE PLAYER SHOULD THROW/AIM AT
+            "stand_position": (-269.918823, -1982.502441, -351.968750), # POSITION WHERE PLAYER SHOULD STAND
+            "throw_position": (-53.481873, -1973.761841, -346.611389), #WHERE PLAYER SHOULD THROW/AIM AT
             "stand_text": "Garage Smoke 2", #STAND TEXT/NAME OF THE LINEUP
             "throw_text": "Jump Throw", #THROW TEXT / TYPE, THIS IS AN JUMP THROW
-            "type_text": "Smoke"  #WHAT TYPE OF GRENADE
+            "type_text": "Smoke",  #WHAT TYPE OF GRENADE
+            "difficulty": "Hard" #DIFFICULTY
         },
         {   #HERE COMES THE NEXT POSITION
-            "stand_position": (-732.876465 -1322.138550 -349.632324), #WHERE PLAYER SHOULD STAND
-            "throw_position": (-85.364807 -1509.874146 -120.726013), #WHERE PLAYER SHOULD THROW
+            "stand_position": (-732.876465, -1322.138550, -349.632324), #WHERE PLAYER SHOULD STAND
+            "throw_position": (-85.364807, -1509.874146, -120.726013), #WHERE PLAYER SHOULD THROW
             "stand_text": "Garage Smoke 1", #STAND TEXT
             "throw_text": "Jump Throw", #THROW TEXT
-            "type_text": "Smoke"  #TYPE OF GRENADE
+            "type_text": "Smoke",  #TYPE OF GRENADE
+            "difficulty": "Easy" #DIFFICULTY
         },
         {   #HERE COMES THE NEXT POSITION
-            "stand_position": (-18.825012 -1463.132812 16.483570), #WHERE PLAYER SHOULD STAND
-            "throw_position": (1038.845703 -286.225525 661.271423), #WHERE PLAYER SHOULD THROW
+            "stand_position": (-18.825012, -1463.132812, 16.483570), #WHERE PLAYER SHOULD STAND
+            "throw_position": (1038.845703, -286.225525, 661.271423), #WHERE PLAYER SHOULD THROW
             "stand_text": "Heaven Smoke", #STAND TEXT
             "throw_text": "Throw", #THROW TEXT
-            "type_text": "Smoke"  #TYPE OF GRENADE
+            "type_text": "Smoke",  #TYPE OF GRENADE
+            "difficulty": "Medium" #DIFFICULTY
         },
         {   #HERE COMES THE NEXT POSITION
-            "stand_position": (37.342712 -1334.752808 -191.968750), #WHERE PLAYER SHOULD STAND
-            "throw_position": (699.019897 -1367.486694 2023.852051), #WHERE PLAYER SHOULD THROW
-            "stand_text": "Small/Blue Box Smoke, #STAND TEXT
+            "stand_position": (37.342712, -1334.752808, -191.968750), #WHERE PLAYER SHOULD STAND
+            "throw_position": (699.019897, -1367.486694, 2023.852051), #WHERE PLAYER SHOULD THROW
+            "stand_text": "Small/Blue Box Smoke", #STAND TEXT
             "throw_text": "Jump Throw", #THROW TEXT
-            "type_text": "Smoke"  #TYPE OF GRENADE
+            "type_text": "Smoke",  #TYPE OF GRENADE
+            "difficulty": "Medium" #DIFFICULTY
         },
     ],
 }
---
+++ vertigo.py
@@ -0,0 +1,35 @@
+"de_vertigo": [ #mapname
+        {
+            "stand_position": (-1423.036621, 693.938232, 11780.415039), # POSITION WHERE PLAYER SHOULD STAND
+            "throw_position": (-1563.797241, 515.481628, 12055.361328), #WHERE PLAYER SHOULD THROW/AIM AT
+            "stand_text": "B Stairs", #STAND TEXT/NAME OF THE LINEUP
+            "throw_text": "Throw", #THROW TEXT / TYPE, THIS IS AN JUMP THROW
+            "type_text": "Smoke",  #WHAT TYPE OF GRENADE
+            "difficulty": "Easy" #DIFFICULTY
+        },
+        {
+            "stand_position": (-1551.968750, 461.748016, 11902.031250), #WHERE PLAYER SHOULD STAND
+            "throw_position": (-2066.931152, 106.380180, 11974.413086), #WHERE PLAYER SHOULD THROW
+            "stand_text": "Mid Smoke", #STAND TEXT
+            "throw_text": "Throw", #THROW TEXT
+            "type_text": "Smoke",  #TYPE OF GRENADE
+            "difficulty": "Easy" #DIFFICULTY
+        },
+        {
+            "stand_position": (-809.031250, -95.031250, 11780.031250), #WHERE PLAYER SHOULD STAND
+            "throw_position": (-893.793213, -496.104980, 11976.213867), #WHERE PLAYER SHOULD THROW
+            "stand_text": "Lane/Scaffold Smoke", #STAND TEXT
+            "throw_text": "Throw", #THROW TEXT
+            "type_text": "Smoke",  #TYPE OF GRENADE
+            "difficulty": "Easy" #DIFFICULTY
+        },
+        {
+            "stand_position": (-919.031250, -78.523438, 11780.031250), #WHERE PLAYER SHOULD STAND
+            "throw_position": (-886.481995, -501.435242, 11985.660156), #WHERE PLAYER SHOULD THROW
+            "stand_text": "Ramp Smoke", #STAND TEXT
+            "throw_text": "Throw", #THROW TEXT
+            "type_text": "Smoke",  #TYPE OF GRENADE
+            "difficulty": "Easy" #DIFFICULTY
+        },
+    ],
+}
